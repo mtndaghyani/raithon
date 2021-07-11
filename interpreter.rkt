6c1156ace3281e0bb8bed807e9ff6f5b5acc9ae7
AmiraliE1380 <60638047+AmiraliE1380@users.noreply.github.com>
2021-07-11 17:51:09 +0430

    Add files via upload
    partially implemented store related functions.

--- interpreter.rkt
+++ interpreter.rkt
@@ -2,6 +2,7 @@
 
 (require "parser.rkt")
 
+;functions relating to environment
 (define (empty-env) '())
 
 (define (extend-env var ref env)
@@ -12,3 +13,33 @@
       (cadr env)
       (apply-env var (cddr env))))
 
+;functions relating to store
+(define (empty-store) '())
+
+(define the-store 'uninitialized)
+
+(define (get-store) the-store)
+
+(define (initialize-store)
+  (set! the-store (empty-store)))
+
+(define (new-ref val)
+  (let ((next-ref (length the-store)))
+    (set! the-store (append the-store (list val)))
+    next-ref))
+
+(define (deref ref)
+  (list-ref the-store ref))
+
+(define (setref ref val)
+  (set! the-store
+        (letrec
+            ((setref-inner
+              (lambda (store1 ref1)
+                      (cond
+                        [(zero? ref1) (cons val (cdr store1))]
+                        [else (cons
+                               (car store1)
+                               (setref-inner (cdr store1) (- ref1 1)))]))))
+          (setref-inner the-store ref))))
+  
