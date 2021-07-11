d3651a6d849b38126be3b4781965267be9f1d996
AmiraliE1380 <60638047+AmiraliE1380@users.noreply.github.com>
2021-07-11 17:15:31 +0430

    Add files via upload
    implemented interpreter

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,1 +1,14 @@
 #lang racket
+
+(require "parser.rkt")
+
+(define (empty-env) '())
+
+(define (extend-env var ref env)
+  (cons var (cons ref env)))
+
+(define (apply-env var env)
+  (if (equal? var (car env))
+      (cadr env)
+      (apply-env var (cddr env))))
+
